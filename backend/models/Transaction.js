74bb01d87526f0d18fc4778274046065af3aade0
RISHI1302 <rishi.ketan.panchal@gmail.com>
2026-08-29 21:06:03 +0530

    Created the Transaction Schema, valition rules and model

---
+++ backend/models/Transaction.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ["income", "expense"],
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+module.exports = Transaction;
