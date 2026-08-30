4bcbb496a29668c08bd1e4d73f58052d31cbd578
RISHI1302 <rishi.ketan.panchal@gmail.com>
2026-08-30 07:33:46 +0530

    Created a summary controller file

---
+++ backend/controllers/summaryController.js
@@ -0,0 +1,11 @@
+const Transaction = require("../models/Transaction.js");
+
+const getSummary = async (req, res) => {
+    
+}
+
+const router = express.Router();
+
+module.exports = {
+    getSummary,
+}
