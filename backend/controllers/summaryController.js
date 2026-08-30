455168c43d546fa134e00ef25f97e7cc17d8acf2
RISHI1302 <rishi.ketan.panchal@gmail.com>
2026-08-30 07:56:29 +0530

    Created the bussiness logic of calculating the total amount, total expense and net balance and tested it successfully

--- backend/controllers/summaryController.js
+++ backend/controllers/summaryController.js
@@ -1,10 +1,39 @@
 const Transaction = require("../models/Transaction.js");
 
 const getSummary = async (req, res) => {
-    
-}
+    try {
+        let totalIncome = 0;
+        let totalExpense = 0;
+        let netBalance = 0;
 
-const router = express.Router();
+        const transactions = await Transaction.find();
+
+        transactions.forEach(transaction => {
+            if (transaction.type == "income") {
+                totalIncome += transaction.amount;
+            } else if (transaction.type == "expense") {
+                totalExpense += transaction.amount;
+            }
+        });
+
+        netBalance = totalIncome - totalExpense;
+
+        res.status(200).json({
+            success: true,
+            data: {
+                totalIncome,
+                totalExpense,
+                netBalance,
+            }
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    };
+}
 
 module.exports = {
     getSummary,
