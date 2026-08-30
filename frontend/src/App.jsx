019fa69eb03a50ff0c3697f944de94bcf678d423
RISHI1302 <rishi.ketan.panchal@gmail.com>
2026-08-30 10:53:49 +0530

    Completed the setup of fetching summary transactions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,34 +5,68 @@ import "./App.css";
 function App() {
 
   const [transactions, setTransactions] = useState([]);
+  const [summary, setSummary] = useState({
+    totalIncome: 0,
+    totalExpense: 0,
+    balance: 0
+  });
 
   useEffect(() => {
 
     fetchTransactions();
+    fetchSummary();
 
   }, []);
 
   const fetchTransactions = async () => {
     try {
 
-      const response = await axios.get(
-        "http://localhost:5000/api/transactions"
-      );
-
+      const response = await axios.get("http://localhost:5000/api/transactions");
+      console.log(response.data);
       setTransactions(response.data.data);
 
     } catch (error) {
-
       console.log(error);
-
     }
-
   };
+
+  const fetchSummary = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/summary"
+      );
+
+      setSummary(response.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <div className="app">
 
       <h1>Income & Expense Ledger</h1>
+
+      <div className="summary">
+
+        <div>
+          <h3>Total Income</h3>
+          <p>₹{summary.totalIncome}</p>
+        </div>
+
+        <div>
+          <h3>Total Expense</h3>
+          <p>₹{summary.totalExpense}</p>
+        </div>
+
+        <div>
+          <h3>Net Balance</h3>
+          <p className={summary.netBalance >= 0 ? "positive" : "negative"}>
+            ₹{summary.balance}
+          </p>
+        </div>
+
+      </div>
 
       <table>
 
